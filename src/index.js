3665670fa69d59101c72e60f2df16845d56d292e
Márcio Costa <marciocst2@gmail.com>
2019-01-01 18:12:46 -0200

    handle initial data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './components/App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { createStore, compose } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import App from './components/App';
+import reducer from './reducers';
+import middleware from './middleware';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducer,
+  composeEnhancers(middleware),
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+document.getElementById('root')
+);
